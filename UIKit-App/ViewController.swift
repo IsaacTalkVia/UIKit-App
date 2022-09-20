734bdf7adbe7501d34172344ac3abfdf7cc3ec35
rayalkira <99301160+IsaacTalkVia@users.noreply.github.com>
2022-09-20 15:14:33 +1000

    Initial Commit

---
+++ UIKit-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-App
+//
+//  Created by TalkVia Mac Mini on 20/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
